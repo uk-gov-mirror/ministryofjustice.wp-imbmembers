f6f2fd7f3660eec5e5bf52418a37cec7a20db696
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-12-02 11:13:19 +0000

    Make the 404 error page more user friendly.

--- web/app/themes/imbmembers/404.php
+++ web/app/themes/imbmembers/404.php
@@ -1,12 +1,19 @@
 <article <?php post_class('entry'); ?>>
   <div class="entry-wrapper">
     <header>
-      <?php get_template_part('templates/page', 'header'); ?>
+      <h1 class="entry-title">Page Not Found</h1>
     </header>
     <div class="entry-content">
       <div class="alert alert-warning">
-        <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+        Sorry, but we couldn't find the page that you're looking for.
       </div>
+      <p>This could have happened for a few reasons:</p>
+      <ul>
+        <li>You may have followed an outdated link.</li>
+        <li>The page you're looking for might have been moved or deleted.</li>
+        <li>There might be a typo in your browser's address bar if you typed the URL.</li>
+      </ul>
+      <p>Please <a href="<?= home_url(); ?>">return to the homepage</a> or use the navigation menu to find what you're looking for.</p>
     </div>
   </div>
 </article>
