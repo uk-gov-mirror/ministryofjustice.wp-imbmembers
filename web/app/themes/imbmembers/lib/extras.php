1dca2e0a06577f0d957dff2b09b738c2faeb3823
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-02-08 17:47:33 +0000

    Fix a bug where show_admin_bar was ignoring the user preference.

--- web/app/themes/imbmembers/lib/extras.php
+++ web/app/themes/imbmembers/lib/extras.php
@@ -7,8 +7,11 @@ use Roots\Sage\Config;
 /**
  * Don't show admin bar for subscribers.
  */
-function show_admin_bar() {
-  return current_user_can('edit_posts');
+function show_admin_bar($show) {
+  if (!current_user_can('edit_posts')) {
+    $show = false;
+  }
+  return $show;
 }
 add_filter('show_admin_bar', __NAMESPACE__ . '\\show_admin_bar');
 
