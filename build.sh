717e9113b3297b946886c2cee2a81dbfb85b126c
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-03-10 14:16:03 +0000

    Add composer auth for CircleCI and bump theme version.

--- build.sh
+++ build.sh
@@ -12,6 +12,21 @@ git clean -xdf
 # Install PHP dependencies (WordPress, plugins, etc.)
 composer install
 
+# Add composer auth file
+if [ ! -z $COMPOSER_USER ] && [ ! -z $COMPOSER_PASS ]
+then
+	cat <<- EOF >> auth.json
+		{
+			"http-basic": {
+				"composer.wp.dsd.io": {
+					"username": "$COMPOSER_USER",
+					"password": "$COMPOSER_PASS"
+				}
+			}
+		}
+	EOF
+fi
+
 # Build theme assets
 cd web/app/themes/imbmembers
 npm install -g bower gulp-cli && echo "{ \"allow_root\": true }" > /root/.bowerrc
@@ -19,4 +34,4 @@ npm install && bower install
 gulp --production
 
 # Remove node_modules and bower_components to (drastically) reduce image size
-rm -Rf node_modules bower_components
+rm -Rf node_modules bower_components auth.json
